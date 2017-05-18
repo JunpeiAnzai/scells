f77733933d1e238b3a2ae591206942d763bf7511
Junpei Anzai <junpei.anzai@gmail.com>
2017-05-18 17:50:33 +0900

    initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "scells"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.scala-lang" % "scala-swing" % "2.11.0-M7"
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.6"
---
+++ src/main/scala/org/stairwaybook/scells/Arithmetic.scala
@@ -0,0 +1,16 @@
+package org.stairwaybook.scells
+
+/**
+  * Created by j_anzai on 2017/05/18.
+  */
+trait Arithmetic { this: Evaluator =>
+  operations += (
+    "add" -> { case List(x, y) => x + y },
+    "sub" -> { case List(x, y) => x - y },
+    "div" -> { case List(x, y) => x / y },
+    "mul" -> { case List(x, y) => x * y },
+    "mod" -> { case List(x, y) => x % y },
+    "sum" -> { xs => (0.0 /: xs)(_ + _) },
+    "prod" -> { xs => (1.0 /: xs)(_ * _) }
+  )
+}
